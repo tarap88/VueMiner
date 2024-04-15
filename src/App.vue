9619cf614577db57ca2991638cdad640b0e43a8c
Tara <taraplatt88@gmail.com>
2024-04-15 13:23:47 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed, ref } from 'vue'
+import { AppState } from './AppState'
+
+const appState = computed(() => AppState)
+
+const cheese = ref(0)
+
+
+function countUp() {
+  console.log('counting');
+  cheese.value++ // the access counter in the script, you need to access the VALUE of counter
+}
+
+</script>
+
+
+
+
+// HTML
+<template>
+  <header class="text-center m-3">
+    <h1> Cheez Clickz </h1>
+  </header>
+
+  <main class="container-fluid">
+    <section class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <img src="https://img.freepik.com/premium-vector/pixel-art-cheese_565656-3.jpg" alt="">
+      </div>
+      <div class="text-center fw-bold mt-3">
+        Cheese Clicks: {{ cheese }}
+        <button class="btn btn-primary" @click="countUp()">+</button>
+      </div>
+    </section>
+  </main>
+</template>
+
+
+
+
+
+
+
+// styles
+<style lang="scss">
+@import "./assets/scss/main.scss";
+</style>
